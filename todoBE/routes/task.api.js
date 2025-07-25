fd9f25fbd73316056becf8193adb5e39eb367e76
CHOSANGOK <chod1510@gmail.com>
2025-07-25 22:03:57 +0900

    routes_updata/delete누락수정

--- todoBE/routes/task.api.js
+++ todoBE/routes/task.api.js
@@ -4,7 +4,7 @@ const router = express.Router();
 
 router.post("/", taskController.createTask);
 router.get("/", taskController.getTask);
-router.put("/:id");
-router.delete("/:id");
+router.put("/:id", taskController.updateTask);
+router.delete("/:id", taskController.deleteTask);
 
 module.exports = router;
