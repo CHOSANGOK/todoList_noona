fec46f5ec9921a758e6ad93dc8c5219b4b7f8702
CHOSANGOK <chod1510@gmail.com>
2025-07-29 04:22:49 +0900

    login_modify

--- todoBE/controller/user.controller.js
+++ todoBE/controller/user.controller.js
@@ -24,19 +24,19 @@ userController.createUser = async (req, res) => {
 userController.login = async (req, res) => {
   try {
     const { email, password } = req.body;
+
     const user = await User.findOne({ email });
 
     if (user) {
-      const isMtach = bcrypt.compareSync(password, user.password);
-      if (isMtach) {
+      const isMatch = bcrypt.compareSync(password, user.password);
+      if (isMatch) {
         const token = user.generateToken();
         return res.status(200).json({ status: "success", user, token });
-      } else {
-        throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
       }
     }
+    throw new Error("아이디 또는 비밀번호가 일치하지 않습니다.");
   } catch (err) {
-    res.status(400).json({ status: "fail", error: err });
+    res.status(400).json({ status: "fail", message: err.message });
   }
 };
 
